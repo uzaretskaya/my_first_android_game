6181222ccd703af219dc3bf2a364fae52293d280
uzaretskaya <JLure@yandex.ru>
2019-03-06 19:17:30 +0300

    ДЗ к уроку 1. Знакомство со средой, добавление картинки.

--- core/src/ru/uzaretskaia/game/MyGame.java
+++ core/src/ru/uzaretskaia/game/MyGame.java
@@ -13,12 +13,11 @@ public class MyGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("space.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
